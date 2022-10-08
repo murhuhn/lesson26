279fe5fb0b66e4b43a2299066176ccb599d4956d
murhuh <murhuh@inbox.ru>
2022-10-08 10:58:51 +0300

    основное задание из урока №26

---
+++ data.js
@@ -0,0 +1,39 @@
+const getData = (url) => {
+  return fetch(url)
+      .then(response => {
+          console.log(`Данные получены ${url}`);
+          return response.json();
+      });
+};
+
+const sendData = ({
+                    url,
+                    data,
+                    method
+                }) => {
+                  
+  return fetch(url, {
+      method: method,
+      body: data,
+      headers: {
+          'Content-type': 'application/json; charset=utf-8'
+      }
+
+  }).then(response => {
+      console.log(`Данные отправлены ${url}`);
+      return response.json();
+  });
+};
+
+getData('db.json')
+  .then(data => {
+      console.log(data);
+      return sendData({
+          url: 'https://jsonplaceholder.typicode.com/posts',
+          data: JSON.stringify(data),
+          method: 'POST'
+      });
+  })
+
+  .then(data => console.log(data))
+  .catch(error => console.log(`Error ${error}`));
